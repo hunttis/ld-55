50ec6eaee485e53fb2183e066ee2df740943c3ea
Ville Hunttis Nuutinen <hunttis@me.com>
2024-04-13 09:31:10 +0300

    Global stuff

--- global.gd
+++ global.gd
@@ -4,3 +4,20 @@ extends Node
 signal sounds_toggled
 
 const NAME_OF_THE_GAME = "The Name of The Game"
+
+enum {
+	AIR_MANA,
+	EARTH_MANA,
+	FIRE_MANA,
+	WATER_MANA
+}
+
+var mana = {
+	AIR_MANA: 0,
+	EARTH_MANA: 0,
+	FIRE_MANA: 0,
+	WATER_MANA: 0
+}
+
+
+
