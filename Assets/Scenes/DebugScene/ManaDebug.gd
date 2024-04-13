ff7f0659823dc337c938ad13ab8506807a20f366
Ville Hunttis Nuutinen <hunttis@me.com>
2024-04-13 13:01:44 +0300

    DebugScene

---
+++ Assets/Scenes/DebugScene/ManaDebug.gd
@@ -0,0 +1,21 @@
+extends VBoxContainer
+
+@onready var air_mana_label: Label = $AirMana
+@onready var water_mana_label: Label = $WaterMana
+@onready var fire_mana_label: Label = $FireMana
+@onready var earth_mana_label: Label = $EarthMana
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (air_mana_label.text != "air_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.AIR], 0.01))):
+		air_mana_label.text = "air_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.AIR], 0.01))
+	if (water_mana_label.text != "water_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.WATER], 0.01))):
+		water_mana_label.text = "water_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.WATER], 0.01))
+	if (fire_mana_label.text != "fire_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.FIRE], 0.01))):
+		fire_mana_label.text = "fire_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.FIRE], 0.01))
+	if (earth_mana_label.text != "earth_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.EARTH], 0.01))):
+		earth_mana_label.text = "earth_mana_label: " + str(snapped(Global.mana[Global.ELEMENT.EARTH], 0.01))
