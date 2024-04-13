c6f55a0416c1afbee33adb5efc11625836edd15f
Mikael Bärlund <mikael.barlund@iki.fi>
2024-04-13 11:54:02 +0300

    speed

--- Assets/Scenes/ElementMeter/ElementalMeter.gd
+++ Assets/Scenes/ElementMeter/ElementalMeter.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 
-@export_range(0,100) var target: float
-@export var perfect_margin: float
-@export var good_margin: float
-@export var medicore_margin: float
-@export var max_mana: float = 2.0
+@export var target: float = 25
+@export var speed: float = 1
+@export var perfect_margin: float = 10
+@export var good_margin: float = 20
+@export var mediocre_margin: float = 40
 @export var meter_type: Global.ELEMENT
 
 @onready var meter: TextureProgressBar = $ProgressBar
@@ -13,9 +13,8 @@ extends Node2D
 
 const METER_WIDTH = 300
 
-
 func _ready():
-	meter.max_value = max_mana
+	meter.max_value = 100
 	meter.value = 0
 	meter.step = 0.01
 	Signals.button_released.connect(_on_button_released)
@@ -24,12 +23,13 @@ func _ready():
 
 func _on_button_released(button_type):
 	if button_type == meter_type:
-		var difference = abs(target - Global.mana[meter_type])
+		var difference = abs(target - meter.value)
+		print(difference, " ", perfect_margin)
 		if difference < perfect_margin:
 			print("PERFECT HIT")
 		elif difference < good_margin:
 			print("GOOD HIT")
-		elif difference < medicore_margin:
+		elif difference < mediocre_margin:
 			print("MEDIOCRE HIT")
 		else:
 			print("MISS")
@@ -37,5 +37,5 @@ func _on_button_released(button_type):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	meter.value = Global.mana[meter_type]
+	meter.value = Global.mana[meter_type]*10*speed
 	
