c292ba49a708d8cbada983e705e90a7307f3a327
Mikael Bärlund <mikael.barlund@iki.fi>
2024-04-13 11:02:40 +0300

    fix

--- main/main.gd
+++ main/main.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 var main_menu_scene: PackedScene = load("res://main/menu/main_menu.tscn")
+var game_scene: PackedScene = load("res://Assets/Scenes/GameScene/GameScene.tscn")
 
 var main_menu
+var game
 
 @onready var current_scene: Node2D  = $CurrentScene
 
 func _init():
 	main_menu = main_menu_scene.instantiate()
+	game = game_scene.instantiate()
 
 
 func _ready():
@@ -23,4 +26,5 @@ func _on_sounds_toggled(toggled_on):
 	
 func _on_play_pressed():
 	current_scene.remove_child(main_menu)
+	current_scene.add_child(game)
 	print("play")	
