abbf36f350602051a0a9a55edd0658c3a803fa98
Ville Hunttis Nuutinen <hunttis@me.com>
2024-04-14 15:48:55 +0300

    Game music and font

--- Assets/Scenes/SoundManager/GameMusic.gd
+++ Assets/Scenes/SoundManager/GameMusic.gd
@@ -1,12 +1,4 @@
 extends AudioStreamPlayer2D
 
-var menumusic_played = false
-
-func _process(_delta):
-	if !playing && menumusic_played:
-		play()
-
 func _on_menu_music_finished():
-	print("Playing game music")
-	menumusic_played = true
 	play()
