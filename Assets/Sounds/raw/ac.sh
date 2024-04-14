d59f4d78459797b6857f3f4aebf9becabb70716d
Mikael Bärlund <mikael.barlund@iki.fi>
2024-04-14 12:16:38 +0300

    sounds

--- Assets/Sounds/raw/ac.sh
+++ Assets/Sounds/raw/ac.sh
@@ -8,12 +8,13 @@ FILES=($(ls | grep '.m4a' | sed -e 's|.m4a||'))
 
 for f in ${FILES[@]}; do
 	echo "Converting $f.m4a to .ogg and .wav"
-	if ffmpeg -i $f.m4a -af "silenceremove=1:0:$T,silenceremove=stop_periods=-1:stop_duration=0.1:stop_threshold=$T" ../$f.wav ../$f.ogg 1>/dev/null 2>/dev/null; then
-		echo "$f.m4a converted"
-		mv $f.m4a converted
+	if ffmpeg -y -i $f.m4a -af "silenceremove=1:0:$T,silenceremove=stop_periods=-1:stop_duration=0.1:stop_threshold=$T" converted/trimmed/$f.wav converted/trimmed/$f.ogg 1>/dev/null 2>/dev/null; then
+		echo "$f.m4a trimmed with noice thershold of $T"
+		if ffmpeg -y -i $f.m4a converted/wav/$f.wav 1>/dev/null 2>/dev/null; then
+			echo "$f.m4a converted"
+			mv $f.m4a converted
+		fi
 	else 
 		echo "$f.m4a failed"
 	fi
 done
-
-echo "All files converted with noice thershold of $T"
