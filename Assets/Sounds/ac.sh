5f039f617dcc904c1439ea57c2d4415dc69a2dd6
Juuso Elo-Rauta <juuso.er@gmail.com>
2024-04-14 14:08:27 +0300

    final revision of the converter script

--- Assets/Sounds/ac.sh
+++ Assets/Sounds/ac.sh
@@ -1,20 +1,74 @@
 #!/bin/bash
 
-INPUT=${1:-"20"}
-T="-"$INPUT"dB"
+function help() {
+	echo "Usage: $0 [-i <input>] [-t <filetype>] [-d <decibel>]"
+	echo "  -i, --input    Input file. If left out, all files of spesified type in the current directory will be converted."
+	echo "  -t, --type     File type to convert from (default: m4a)"
+	echo "  -d, --decibel  Decibel level to trim (default: 20)"
+	exit 0
+}
 
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+	-i | --input)
+		INPUT="$2"
+		shift
+		;;
+	-t | --type)
+		TYPE="$2"
+		shift
+		;;
+	-d | --decibel)
+		DECIBEL="$2"
+		shift
+		;;
+	-h | --help)
+		help
+		;;
+	*)
+		echo "Unknown parameter passed: $1"
+		exit 1
+		;;
+	esac
+	shift
+done
 
-FILES=($(ls | grep '.m4a' | sed -e 's|.m4a||'))
+mkdir -p converted/trimmed/{ogg,wav}
+mkdir -p converted/original/{ogg,wav}
+
+if [ -z "$TYPE" ]; then
+	TYPE="m4a"
+fi
+
+if [ -z "$DECIBEL" ]; then
+	DECIBEL="20"
+fi
+
+T="-"$DECIBEL"dB"
+
+if [ -z "$INPUT" ]; then
+	FILES=($(ls | grep ".$TYPE" | sed -e "s|.$TYPE||"))
+else
+	FILES=($INPUT)
+fi
+
+if [ -z "$INPUT" ]; then
+	read -p "No input file provided. Convert all files of type .$TYPE in current directory? (Y/n) " -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		exit 0
+	fi
+fi
 
 for f in ${FILES[@]}; do
-	echo "Converting $f.m4a to .ogg and .wav"
-	if ffmpeg -y -i $f.m4a -af "silenceremove=1:0:$T,silenceremove=stop_periods=-1:stop_duration=0.1:stop_threshold=$T" converted/trimmed/$f.wav converted/trimmed/$f.ogg 1>/dev/null 2>/dev/null; then
-		echo "$f.m4a trimmed with noice thershold of $T"
-		if ffmpeg -y -i $f.m4a converted/wav/$f.wav 1>/dev/null 2>/dev/null; then
-			echo "$f.m4a converted"
-			mv $f.m4a converted
+	echo "Converting $f.$TYPE to .ogg and .wav"
+	if ffmpeg -y -i $f.$TYPE -af "silenceremove=1:0:$T,silenceremove=stop_periods=-1:stop_duration=0.1:stop_threshold=$T" converted/trimmed/wav/$f.wav converted/trimmed/ogg/$f.ogg 1>/dev/null 2>/dev/null; then
+		echo "$f.$TYPE trimmed with noice thershold of $T"
+		if ffmpeg -y -i $f.$TYPE converted/original/wav/$f.wav converted/original/ogg/$f.ogg 1>/dev/null 2>/dev/null; then
+			echo "$f.$TYPE converted"
+			mv $f.$TYPE converted
 		fi
-	else 
-		echo "$f.m4a failed"
+	else
+		echo "$f.$TYPE failed"
 	fi
 done
