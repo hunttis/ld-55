e2d7c9079fe2d8e781b9e9928aa13924c3769a09
Juuso Elo-Rauta <juuso.er@gmail.com>
2024-04-14 11:52:17 +0300

    more conversion script refinement

--- Assets/Sounds/raw/ac.sh
+++ Assets/Sounds/raw/ac.sh
@@ -8,8 +8,9 @@ FILES=($(ls | grep '.m4a' | sed -e 's|.m4a||'))
 
 for f in ${FILES[@]}; do
 	echo "Converting $f.m4a to .ogg and .wav"
-	if ffmpeg -i $f.m4a -af "silenceremove=1:0:$T,silenceremove=stop_periods=-1:stop_duration=0.1:stop_threshold=$T" $f.wav $f.ogg 1>/dev/null 2>/dev/null; then
+	if ffmpeg -i $f.m4a -af "silenceremove=1:0:$T,silenceremove=stop_periods=-1:stop_duration=0.1:stop_threshold=$T" ../$f.wav ../$f.ogg 1>/dev/null 2>/dev/null; then
 		echo "$f.m4a converted"
+		mv $f.m4a converted
 	else 
 		echo "$f.m4a failed"
 	fi
