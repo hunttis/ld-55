4d993951dd9f5cf488e69822e8d22ec119d071bf
Ville Hunttis Nuutinen <hunttis@me.com>
2024-04-14 19:36:15 +0300

    Running liquids

--- Assets/Scenes/VFXManager/LiquidVFX/LiquidVFX.gd
+++ Assets/Scenes/VFXManager/LiquidVFX/LiquidVFX.gd
@@ -2,12 +2,24 @@ extends Node2D
 
 var destroy_timer = 3
 var queued_destroy = false
+var element = null
 
 func _ready():
 	for particles in get_children():
 		particles.emitting = true
 
 func _process(_delta):
+	if element:
+		match element:
+			Global.VFX.APPLE_JUICE:
+				modulate = Color.YELLOW
+			Global.VFX.ONION_JUICE:
+				modulate = Color.WHITE
+			Global.VFX.PEAR_JUICE:
+				modulate = Color.GREEN
+			Global.VFX.ORANGE_JUICE:
+				modulate = Color.ORANGE
+
 	destroy_timer -= _delta
 	
 	if queued_destroy:
@@ -23,8 +35,5 @@ func _process(_delta):
 			queued_destroy = true
 			destroy_timer = particles.lifetime * 2
 
-	# if Input.is_action_just_pressed("ui_accept"):
-	# 	for particles: CPUParticles2D in get_children():
-	# 		particles.emitting = false
 
 
