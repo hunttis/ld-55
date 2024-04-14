900315d299643a780c1091cf8c50beafa763e2ae
Juuso Elo-Rauta <juuso.er@gmail.com>
2024-04-14 11:25:11 +0300

    refined converter script

--- Assets/Sounds/ac.sh
+++ Assets/Sounds/ac.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-FOLDER=${1:""}
+INPUT=${1:-"20"}
+T="-"$INPUT"dB"
 
-FILES=($(ls $1 | grep '.m4a' | sed -e 's|.m4a||'))
+
+FILES=($(ls | grep '.m4a' | sed -e 's|.m4a||'))
 
 for f in ${FILES[@]}; do
 	echo "Converting $f.m4a to .ogg and .wav"
-	if ffmpeg -i $f.m4a $f.ogg $f.wav 1>/dev/null 2>&1; then
+	if ffmpeg -i $f.m4a -af "silenceremove=1:0:$T,silenceremove=stop_periods=-1:stop_duration=0.1:stop_threshold=$T" $f-trimmed.wav $f-trimmed.ogg 1>/dev/null 2>/dev/null; then
 		echo "$f.m4a converted"
 	else 
 		echo "$f.m4a failed"
 	fi
 done
+
+echo "All files converted with noice thershold of $T"
