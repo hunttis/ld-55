a40f52fa147ba54861ac74aec9e832138deacf26
Juuso Elo-Rauta <juuso.er@gmail.com>
2024-04-15 16:39:14 +0300

    added new main menu background and difficulty selection

--- difficulty.gd
+++ difficulty.gd
@@ -9,48 +9,51 @@ var meter_speed: float = 4
 var meter_speed_multiplier: float = 1.10
 
 enum DIFFICULTY {
-    NONE,
-    PROGRESSIVE,
-    PRETTY_HARD,
-    UNFAIR,
+	NONE,
+	PROGRESSIVE,
+	PRETTY_HARD,
+	UNFAIR,
 }
 
 var progressive = {
-    current_difficulty = 1,
-    progression = [1,2,2],
-    speed_up_start_round = 5,
-    meter_speed = 4,
-    meter_speed_multiplier = 1.10,
+	title = "Progressive",
+	current_difficulty = 1,
+	progression = [1,2,2],
+	speed_up_start_round = 5,
+	meter_speed = 4,
+	meter_speed_multiplier = 1.10,
 }
 
 var pretty_hard = {
-    current_difficulty = 4,
-    progression = [],
-    speed_up_start_round = 100,
-    meter_speed = 6,
-    meter_speed_multiplier = 1.02,
+	title = "Pretty Hard",
+	current_difficulty = 4,
+	progression = [],
+	speed_up_start_round = 100,
+	meter_speed = 6,
+	meter_speed_multiplier = 1.02,
 }
 
 var unfair = {
-    current_difficulty = 4,
-    progression = [],
-    speed_up_start_round = 1,
-    meter_speed = 5.5,
-    meter_speed_multiplier = 1.4,
+	title = "Unfair",
+	current_difficulty = 4,
+	progression = [],
+	speed_up_start_round = 1,
+	meter_speed = 5.5,
+	meter_speed_multiplier = 1.4,
 }
 
 var difficulties = {
-    DIFFICULTY.PROGRESSIVE: progressive,
-    DIFFICULTY.PRETTY_HARD: pretty_hard,
-    DIFFICULTY.UNFAIR: unfair,
+	DIFFICULTY.PROGRESSIVE: progressive,
+	DIFFICULTY.PRETTY_HARD: pretty_hard,
+	DIFFICULTY.UNFAIR: unfair,
 }
 
 func _ready():
-    Signals.set_difficulty.connect(_on_set_difficulty)
+	Signals.set_difficulty.connect(_on_set_difficulty)
 
 func _on_set_difficulty(difficulty: DIFFICULTY):
-    current_difficulty = difficulties[difficulty].current_difficulty
-    progression = difficulties[difficulty].progression
-    speed_up_start_round = difficulties[difficulty].speed_up_start_round
-    meter_speed = difficulties[difficulty].meter_speed
-    meter_speed_multiplier = difficulties[difficulty].meter_speed_multiplier
+	current_difficulty = difficulties[difficulty].current_difficulty
+	progression = difficulties[difficulty].progression
+	speed_up_start_round = difficulties[difficulty].speed_up_start_round
+	meter_speed = difficulties[difficulty].meter_speed
+	meter_speed_multiplier = difficulties[difficulty].meter_speed_multiplier
--- main/menu/main_menu.gd
+++ main/menu/main_menu.gd
@@ -1,12 +1,18 @@
 extends Control
 
 @onready var sound_toggle: CheckButton = $MenuItems/ItemsContainer/VBoxContainer/SoundToggle
+@onready var difficulty_option: OptionButton = $MenuItems/ItemsContainer/VBoxContainer/DifficultyOption
 
 func _ready():
 	print("main_menu ready")
 	Signals.after_sounds_toggled.connect(_on_toggle_sounds)
 	sound_toggle.set_pressed_no_signal(true)
 	Sounds.play_sound.emit(Sounds.EFFECT.OPEN)
+	difficulty_option.item_selected.connect(_on_difficulty_selected)
+	for key in Difficulty.difficulties:
+		difficulty_option.add_item(Difficulty.difficulties[key].title, key)
+		if key == Difficulty.current_difficulty:
+			difficulty_option.select(difficulty_option.get_item_count() - 1)
 
 func _process(_delta):
 	if !get_viewport().gui_get_focus_owner():
@@ -16,3 +22,8 @@ func _on_toggle_sounds():
 	var bus_idx = AudioServer.get_bus_index("Master")
 	var mute = AudioServer.is_bus_mute(bus_idx)
 	sound_toggle.set_pressed_no_signal(!mute)
+
+func _on_difficulty_selected(index):
+	var difficulty = difficulty_option.get_item_id(index)
+	print("difficulty selected: ", difficulty)
+	Signals.set_difficulty.emit(difficulty)
